7c65c1f633af5bcb5feee7ae3149e61a7867df85
Dinolind <110360785+Dinolind@users.noreply.github.com>
2023-06-04 01:05:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function onclickfunction(){
   if (userGuess.toUpperCase() == "093640263"){
     document.getElementById('inputField').style.outline='2px solid #4efda6'
     document.getElementById('btn').style.background='#4efda6';
-    responseText.innerHTML = "Claim your second ticket role using <a href='#'>this message</a> and then check the channel list once again";
+    responseText.innerHTML = "Claim your second ticket role using <a href='https://discord.com/channels/1105201628707029012/1105201628707029015/1105225466463391744'>this message</a> and then check the channel list once again";
   }
   else if (userGuess.toUpperCase() == "33"){
     document.getElementById('inputField').style.outline='2px solid #fd4e4e'
@@ -31,12 +31,12 @@ function onclickfunction(){
   else if (userGuess.toUpperCase() == "OVERBOARD"){
     document.getElementById('inputField').style.outline='2px solid #fdf14e'
     document.getElementById('btn').style.background='#fdf14e';
-    responseText.innerHTML = "Claim your first ticket role using <a href='#'>this message</a> and then check the channel list";
+    responseText.innerHTML = "Claim your first ticket role using <a href='https://discord.com/channels/1105201628707029012/1105201628707029015/1105225466463391744'>this message</a> and then check the channel list";
   }
   else {
     document.getElementById('inputField').style.outline='2px solid #fd4e4e'
     document.getElementById('btn').style.background='#fd4e4e';
-    responeText.innerText = "Incorrect, Please Try Again";
+    responseText.innerText = "Incorrect, Please Try Again";
   }
 
 }
